be882fc55450ee3189d2ba554acec079a8c444d3
dabeng <dabeng413@gmail.com>
2018-10-21 10:22:14 +0800

    first patch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h2>{{ msg }}</h2>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
